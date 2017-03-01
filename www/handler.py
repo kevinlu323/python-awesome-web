89610db484063c5c8eca743fce3bef3dfea35eec
kevinlu323 <kevinlu323@gmail.com>
2017-03-01 10:06:05 -0600

    Add user logout functionality

--- www/handler.py
+++ www/handler.py
@@ -128,3 +128,12 @@ async def authenticate(*, email, passwd):
     r.content_type = 'application/json'
     r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
     return r
+
+
+@get('/logout')
+async def logout(request):
+    referer = request.headers.get('Referer')
+    r = web.HTTPFound(referer or '/')
+    r.set_cookie(COOKIE_NAME, '-deleted', max_age=0, httponly=True)
+    logging.info('user logged out!')
+    return r
